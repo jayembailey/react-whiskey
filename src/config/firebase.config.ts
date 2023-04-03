4c5d4a4a8bbca99712566d6294b84179d2c3bb81
Jacob Bailey <jayembailey@gmail.com>
2023-04-03 10:08:00 -0400

    initial commit for firebase hosting

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDZdM-GVtTchJFUHNtsTM4ZY6u-AyTFaBQ",
+  authDomain: "whiskey-collection-61d99.firebaseapp.com",
+  projectId: "whiskey-collection-61d99",
+  storageBucket: "whiskey-collection-61d99.appspot.com",
+  messagingSenderId: "95004594393",
+  appId: "1:95004594393:web:13e7979aba3024c56baec8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
